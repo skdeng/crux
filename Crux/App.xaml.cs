369d66eefefaf7a6f010441e2f6bdefac9a45ae8
Shao Kun Deng <deng_shaokun@hotmail.com>
2017-04-30 19:41:44 -0400

    Change from console to GUI with WPF

---
+++ Crux/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace Crux
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
--- Crux/Program.cs
+++ Crux/Program.cs
@@ -1,30 +1,26 @@
-﻿using Crux.BasicStrategy;
-using Crux.OkcoinREST;
-using System;
-
-namespace Crux
+﻿namespace Crux
 {
     class Program
     {
         static void Main(string[] args)
         {
-            while (true)
-            {
-                try
-                {
-                    Log.LogExportFile = $"{DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss")}.txt";
-                    Log.LogLevel = 2;
-                    var okcAPI = new OKCMarketRESTAPI("../../../Keys/okc.txt", "LTC/USD");
-                    //Thread.Sleep(5000);
-                    MeanReversalStrategy meanReversalStrategy = new MeanReversalStrategy(okcAPI, 3600000 / 4, 12);
-                    meanReversalStrategy.Start(false);
-                }
-                catch (Exception e)
-                {
-                    Log.Write(e.Message, 0);
-                    Log.Write(e.StackTrace, 0);
-                }
-            }
+            //while (true)
+            //{
+            //    try
+            //    {
+            //        Log.LogExportFile = $"{DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss")}.txt";
+            //        Log.LogLevel = 2;
+            //        var okcAPI = new OKCMarketRESTAPI("../../../Keys/okc.txt", "LTC/USD");
+            //        //Thread.Sleep(5000);
+            //        MeanReversalStrategy meanReversalStrategy = new MeanReversalStrategy(okcAPI, 3600000 / 4, 12);
+            //        meanReversalStrategy.Start(false);
+            //    }
+            //    catch (Exception e)
+            //    {
+            //        Log.Write(e.Message, 0);
+            //        Log.Write(e.StackTrace, 0);
+            //    }
+            //}
         }
     }
 }
